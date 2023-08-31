f9668c1bd782397bfa79dbc9ae82e33de3efa9fe
Rolando Garcia Cutie <rolando.garcia@alivi.com>
2023-08-31 14:38:08 -0400

    Añadiendo algunos codigos a hereocomponent

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-heroe',
   templateUrl: './heroe.component.html',
-  styleUrls: ['./heroe.component.css']
+  styleUrls: ['./heroe.component.css'],
 })
 export class HeroeComponent {
+  public name: string = 'IronMan';
+  public age: number = 2;
 
+  get capitalazedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  heroeDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero(): void {
+    this.name = 'Spiderman';
+  }
+
+  changeAge(): void {
+    this.age = 23;
+  }
+  resetForm() {
+    this.name = 'IronMan';
+    this.age = 2;
+  }
 }
